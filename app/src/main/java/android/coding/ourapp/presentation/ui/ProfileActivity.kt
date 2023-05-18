40927068cc5a32325328ea400b5608b6fd683920
Faizalilham <faizalfalakh19@gmail.com>
2023-05-19 01:46:47 +0700

    Feature: Profile

--- app/src/main/java/android/coding/ourapp/presentation/ui/ProfileActivity.kt
+++ app/src/main/java/android/coding/ourapp/presentation/ui/ProfileActivity.kt
@@ -1,17 +1,48 @@
 package android.coding.ourapp.presentation.ui
 
 import android.coding.ourapp.databinding.ActivityProfileBinding
+import android.coding.ourapp.presentation.viewmodel.auth.AuthViewModel
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ProfileActivity : AppCompatActivity() {
     private var _binding : ActivityProfileBinding? = null
     private val binding get() = _binding!!
+    private val authViewModel by viewModels<AuthViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        currentUser()
+        doLogout()
 
+    }
+
+    private fun currentUser(){
+        binding.apply {
+            tvEmail.text = authViewModel.currentUser?.email
+            tvSchool.text = authViewModel.currentUser?.displayName
+        }
+    }
+
+    private fun doLogout(){
+        binding.cardLogout.setOnClickListener {
+            startActivity(Intent(this,LoginActivity::class.java).also{
+                authViewModel.deleteToken()
+                finish()
+            })
+            finish()
+        }
+    }
+
+    private fun moveToHome(){
+        binding.imageBack.setOnClickListener {
+            finish()
+        }
     }
 
     override fun onDestroy() {
